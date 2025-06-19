b0c342c3c6da03b2d55b1bd1fab60c569f029436
ipalv9 <ipal7271@gmail.com>
2025-06-19 23:32:07 +0700

    membuat migrate dan laravel ui

---
+++ app/Models/pemesanans.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class pemesanans extends Model
+{
+    use HasFactory;
+}
---
+++ database/migrations/2025_06_19_154614_create_pemesanans_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pemesanans', function (Blueprint $table) {
+            $table->id();
+            $table->integer('jumlah');
+            $table->date('tgl_tiket');
+            $table->bigInteger('id_tarif');
+            $table->bigInteger('id_karyawan');
+            $table->enum('status',['pending','berhasil','pengiriman']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pemesanans');
+    }
+};
